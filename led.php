bc5a684471c97df1d51085ab572b5f4bda4345de
tomotomo <tomotomobile@gmail.com>
2018-12-17 17:51:20 +0900

    show user

--- led.php
+++ led.php
@@ -1,11 +1,13 @@
 <?php
+echo 'I am ' . posix_getpwuid(posix_geteuid())['name'].PHP_EOL;
+
 define('GPIO', '/sys/class/gpio');
 define('GPIO4', GPIO.'/gpio4');
 file_put_contents(GPIO.'/export', 4);
 file_put_contents(GPIO4.'/direction','out');
-echo "ON" . PHP_EOL;
+echo 'ON' . PHP_EOL;
 file_put_contents(GPIO4.'/value', 1);
 sleep(5);
-echo "OFF" . PHP_EOL;
+echo 'OFF' . PHP_EOL;
 file_put_contents(GPIO4.'/value', 0);
 file_put_contents(GPIO.'/unexport', 4);
